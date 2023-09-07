9af94d1cdeb1b0a8dbc991a196f98965cb6f1671
Sergii Marchenko <marikmarchelo@gmail.com>
2023-09-07 21:20:52 +0300

    new

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-fetch("https://api.ipify.org?format=json")
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
+const formEl = document.querySelector(".box");
+
+formEl.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(formEl.elements.login.value);
+
+  const login = formEl.elements.login.value;
+
+  fetch(`https://api.github.com/users/${login}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+}
